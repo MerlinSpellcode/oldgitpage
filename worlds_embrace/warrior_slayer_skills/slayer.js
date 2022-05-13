31120457552f4102b8911a4605760fa510351e2d
MerlinCaster <joao.merlin@digitalpages.com.br>
2022-05-13 02:57:29 -0300

    world embrace, warrior skills

--- worlds_embrace/warrior_slayer_skills/slayer.js
+++ worlds_embrace/warrior_slayer_skills/slayer.js
@@ -3,26 +3,108 @@ slayer_skills = [
         "icon": "stab.png",
         "type": "move",
         "name": "Perfurar",
-        "lvl_max": "5",
         "description": {
             "cooldown": "5s",
             "casting": "0.2s",
             "cost": "20 stamina",
-            "effect": "Prefura inimigo...",
+            "effect": "Prefura inimigo com grande potencia, ignorando 10%(+4% p/lvl) da armadura inimiga e quebrando bloqueios, causando 100% de dano.",
             "anim": "stab.gif"
         }
     },
     {
-        "icon": "stab.png",
+        "icon": "slash.png",
         "type": "move",
-        "name": "Perfurar",
-        "lvl_max": "5",
+        "name": "Corte Carregado",
         "description": {
-            "cooldown": "5s",
-            "casting": "0.2s",
+            "cooldown": "15s",
+            "casting": "1.5s",
             "cost": "20 stamina",
-            "effect": "Prefura inimigo...",
-            "anim": "stab.gif"
+            "effect": "Carrega por dois segundos um golpe horizontal, que causa 130% + (10% p/lvl) ",
+            "anim": "slash.gif"
+        }
+    },
+    {
+        "icon": "respirar_fundo.png",
+        "type": "move",
+        "name": "Respirar Fundo",
+        "description": {
+            "cooldown": "20s",
+            "casting": "0s",
+            "cost": "20 stamina",
+            "effect": "Recupera 10% da vida perdida (+2% p/lvl) e 20 (+4 p/lvl) de stamina Instantaneamente",
+            "anim": ""
+        }
+    },
+    {
+        "icon": "golpes_concentrados.png",
+        "type": "buff",
+        "name": "Golpes concentrados",
+        "description": {
+            "cooldown": "8s",
+            "casting": "0s",
+            "cost": "0 stamina",
+            "effect": "Durante 5s, Golpes leves causam 10% (2% p/lvl) de dano adicional e queimam -10 de stamina inimiga por golpe, porem, cada golpe seu gasta +10 stamina adicional",
+            "anim": ""
+        }
+    },
+    {
+        "icon": "jump_attack.png",
+        "type": "move",
+        "name": "Ataque com Salto",
+        "description": {
+            "cooldown": "18s",
+            "casting": "0.5s",
+            "cost": "30 stamina",
+            "effect": "Salta a frente rapidamente, chocando sua espada com o chão causando 70% (+4% p/lvl) de dano e reduzindo -20 de stamina do inimigo",
+            "anim": "jump_attack_1.gif"
+        }
+    },
+    {
+        "icon": "upper.png",
+        "type": "move",
+        "name": "Corte Gancho",
+        "description": {
+            "cooldown": "8s",
+            "casting": "0.2s",
+            "cost": "30 stamina",
+            "effect": "Investe contra o inimigo, cortando de baixo para cima, causando 90% (+4% p/lvl) de dano e derrubando o alvo",
+            "anim": "upper.gif"
+        }
+    },
+    {
+        "icon": "grito.png",
+        "type": "move",
+        "name": "Grito Intimidador",
+        "description": {
+            "cooldown": "40s",
+            "casting": "0.2s",
+            "cost": "40 stamina",
+            "effect": "Grita tão alto que prejudica até mesmo seu pulmão e garganta, causando Fear de 1s (+0.2 p/lvl) em todos inimigos ao redor, porém, perdendo 20% da vida atual",
+            "anim": "grito.gif"
+        }
+    },
+    {
+        "icon": "parry.png",
+        "type": "move",
+        "name": "Contra-Golpe",
+        "description": {
+            "cooldown": "10s",
+            "casting": "0s",
+            "cost": "10 stamina",
+            "effect": "Assume uma postura defensiva por 2s, o primeiro golpe recebido causara 30% (+6% p/lvl) a menos de dano em você, em seguida contra-golpeará causando 100% (+6% p/lvl) de dano a frente",
+            "anim": "parry.gif"
+        }
+    },
+    {
+        "icon": "wolf.png",
+        "type": "ULTIMATE",
+        "name": "Postura do Lobo",
+        "description": {
+            "cooldown": "180s",
+            "casting": "1s",
+            "cost": "0 stamina",
+            "effect": "assume uma postura de combate diferente, offensiva. </br> Recebe 20% a mais de dano </br> Seus ataques saem 30% mais rápido </br> Você recebe um dano adicional de 20% </br> Seu roll se torna um pequeno dash, reduzindo o custo de stamina para X </br>",
+            "anim": "wolf.gif"
         }
     }
 ]
