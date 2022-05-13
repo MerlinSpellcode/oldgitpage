9fe0016ec8a9b8aca56184a615c0a63b610c23b9
MerlinCaster <joao.merlin@digitalpages.com.br>
2022-05-13 01:12:06 -0300

    world embrace

---
+++ worlds_embrace/warrior_slayer_skills/slayer.js
@@ -0,0 +1,28 @@
+slayer_skills = [
+    {
+        "icon": "stab.png",
+        "type": "move",
+        "name": "Perfurar",
+        "lvl_max": "5",
+        "description": {
+            "cooldown": "5s",
+            "casting": "0.2s",
+            "cost": "20 stamina",
+            "effect": "Prefura inimigo...",
+            "anim": "stab.gif"
+        }
+    },
+    {
+        "icon": "stab.png",
+        "type": "move",
+        "name": "Perfurar",
+        "lvl_max": "5",
+        "description": {
+            "cooldown": "5s",
+            "casting": "0.2s",
+            "cost": "20 stamina",
+            "effect": "Prefura inimigo...",
+            "anim": "stab.gif"
+        }
+    }
+]
