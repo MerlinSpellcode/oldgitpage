981082ed1a89f0dc2cb4be24581999385e661de6
MerlinCaster <joao.merlin@digitalpages.com.br>
2022-05-15 15:34:29 -0300

    Knight Full

---
+++ worlds_embrace/knight/captain_skills/skills.js
@@ -0,0 +1,110 @@
+the_skills = [
+    {
+        "icon": "stabs.png",
+        "type": "move",
+        "name": "Tatician Piercing",
+        "description": {
+            "cooldown": "3s",
+            "casting": "0s",
+            "cost": "10 stamina",
+            "effect": "Prefura inimigo rapidamenteo, causando 50% (+5% p/lvl) de dano TRHUST, e causando vulnerabilidade a dano físico no inimigo de 5% por 5s, acumulando até 3x chegando a 15% de vulnerabilidade. ",
+            "anim": ""
+        }
+    },
+    {
+        "icon": "pommel.png",
+        "type": "move",
+        "name": "Pommel Strike",
+        "description": {
+            "cooldown": "25s",
+            "casting": "0s",
+            "cost": "10 stamina",
+            "effect": "Bate com o pomo da espada na cabeça do inimigo, causando 40% (+5% p/lvl) de dano STRIKE, atordoando o alvo por 1s (+0.2s p/lvl). MOB atigindo pelo Pomel recebe Taunt por 5s",
+            "anim": ""
+        }
+    },
+    {
+        "icon": "warcry.png",
+        "type": "move",
+        "name": "Warcry",
+        "description": {
+            "cooldown": "30s",
+            "casting": "0s",
+            "cost": "20 stamina",
+            "effect": "Da um grito forte de guerra, retira o controle de todos aliados em uma área de 2m (+0.5m p/lvl) e concede um buff de 10% de dano. Inimigos MOB dentro da área recebem efeito de Taunt por 4s ",
+            "anim": ""
+        }
+    },
+    {
+        "icon": "taunt.png",
+        "type": "buff",
+        "name": "Captain Taunt",
+        "description": {
+            "cooldown": "60s",
+            "casting": "1s",
+            "cost": "20 stamina",
+            "effect": "Provoca Inimigos em um raio de 3m (+1m p/lvl), Mobs recebem efeito de TAUNT por 5s, Players Inimigos são forçados a caminhar na sua direção por 2s",
+            "anim": ""
+        }
+    },
+    {
+        "icon": "shield_parry.png",
+        "type": "move",
+        "name": "Shield Parry",
+        "description": {
+            "cooldown": "10s",
+            "casting": "0s",
+            "cost": "10 stamina",
+            "effect": "Pelos proximos 5s, o inimigo que que tiver um ataque bloqueado pelo seu escudo, fica atordoado por 0.5s (+0.2s p/lvl)",
+            "anim": ""
+        }
+    },
+    {
+        "icon": "hamstring.png",
+        "type": "move",
+        "name": "Hamstring",
+        "description": {
+            "cooldown": "15s",
+            "casting": "0s",
+            "cost": "20 stamina",
+            "effect": "Golpea o inimigo nas pernas, causando 70% (+2% p/lvl) de dano SLASH, causando SLOW de 20% (+5% p/lvl) no inimigo",
+            "anim": ""
+        }
+    },
+    {
+        "icon": "assemble.png",
+        "type": "move",
+        "name": "Assemble",
+        "description": {
+            "cooldown": "60s",
+            "casting": "0.2s",
+            "cost": "40 stamina",
+            "effect": "Encoraja seus aliados proximos a uma distancia de 3m (+1m p/lvl), consedendo-lhes BUFF de aumento de velocidade de movimento em 20% (+2% p/lvl) e regen de stamina em 30%",
+            "anim": ""
+        }
+    },
+    {
+        "icon": "shieldrush.png",
+        "type": "move",
+        "name": "Shield Rush",
+        "description": {
+            "cooldown": "40s",
+            "casting": "1s",
+            "cost": "40 stamina",
+            "effect": "Posiciona o escudo a frente e avança 2m (+1m p/lvl), causando KNOCKDOWN em todos inimigos em seu caminho.",
+            "anim": ""
+        }
+    },
+    {
+        "icon": "raidcaptainpennant.png",
+        "type": "ULTIMATE",
+        "name": "Raid Captain Pennant",
+        "description": {
+            "cooldown": "180s",
+            "casting": "2s",
+            "cost": "0 stamina",
+            "effect": "Coloca uma bandeira no campo de batalha, com o emblema de sua Guilda, que permanece por 10s. </br> * Aliados dentro da bandeira recebem BUFF de 20% de dano </br> * Aliados dentro da bandeira recebem BUFF de regen de Stamina em 10 a cada 2s </br> * Aliados dentro da bandeira recebem BUFF de 10% na taxa crítica.</br>",
+            "anim": ""
+        }
+    }
+]
